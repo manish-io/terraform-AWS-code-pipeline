6ab9c07606b7708cd51d83357d2b72f65a1fd937
Manish <164000555+manish-io@users.noreply.github.com>
2024-06-28 13:15:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     ]
   })
 }
+
 resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline-role"
 
@@ -48,28 +49,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
-resource "aws_codebuild_project" "build_project" {
-  name          = "static-website-build"
-  service_role  = aws_iam_role.codepipeline_role.arn
-  build_timeout = "5"
 
-  source {
-    type      = "GITHUB"
-    location  = "https://github.com/manish-io/terraform-AWS-code-pipeline.git"
-    buildspec = "buildspec.yml"
-  }
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:4.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
-  }
-}
 resource "aws_codepipeline" "website_pipeline" {
   name     = "website-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
@@ -106,7 +86,7 @@ resource "aws_codepipeline" "website_pipeline" {
       category        = "Deploy"
       owner           = "AWS"
       provider        = "S3"
-      input_artifacts = ["build_output"]
+      input_artifacts = ["source_output"]
       version         = "1"
       configuration = {
         BucketName = aws_s3_bucket.website_bucket.bucket
