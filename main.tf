f5040d0d68989ef7d7ec319e7b883bd79dcd62d0
Manish <164000555+manish-io@users.noreply.github.com>
2024-06-28 15:31:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "my-unique-bucket-name-terrafor-code-pipeline-1"
+  bucket = "my-unique-bucket-name-terrafor-code-pipeline-1-1"
 
   website {
     index_document = "index.html"
